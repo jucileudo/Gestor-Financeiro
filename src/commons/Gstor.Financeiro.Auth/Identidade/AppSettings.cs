3363117c7fe0b22dd8725604c7c09b408688d886
Jucileudo Lima <juci0310@hotmail.com>
2022-05-01 10:46:46 -0300

    Implementado Api de Autenticação e Autorização usando Microsoft Identity

--- src/commons/Gstor.Financeiro.Auth/Identidade/AppSettings.cs
+++ src/commons/Gstor.Financeiro.Auth/Identidade/AppSettings.cs
@@ -2,6 +2,9 @@
 { 
     public class AppSettings
     {
-        public string AutenticacaoJwksUrl { get; set; }
+        public string Secret { get; set; }
+        public int ExpiracaoHoras { get; set; }
+        public string Emissor { get; set; }
+        public string ValidoEm { get; set; }
     }
 }
